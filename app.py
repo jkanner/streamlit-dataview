dab30c2ac6cfa31eba2b1539ea9113c8200f6f81
Jonah Kanner <jkanner@caltech.edu>
2020-07-23 09:44:48 -0700

    Now select by event

--- app.py
+++ app.py
@@ -3,71 +3,80 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 import requests, os
 from gwpy.timeseries import TimeSeries
 from gwosc.locate import get_urls
+from gwosc import datasets
+
+# -- Default detector list
+detectorlist = ['H1','L1', 'V1']
 
 # Title the app
-st.title('GWpy Plotting App')
+st.title('Gravitational Wave Quickview App')
 
+st.markdown("""
+ * Use the controls at left to select data
+ * Learn more at https://gw-openscience.org
+""")
 
 @st.cache   #-- Magic command to cache data
 def load_gw(t0, detector):
-
-    # Commented out code for alternate way to access data
-    #url = get_urls(detector, t0, t0)[-1]
-    #print(url)
-    #fn = os.path.basename(url)
-    
-    #with open(fn,'wb') as strainfile:                 
-    #    straindata = requests.get(url)
-    #    strainfile.write(straindata.content)
-
-    # -- Read strain data
-    # strain = TimeSeries.read(fn,format='hdf5.losc')
-
-    # -- Clean-up temporary file
-    # os.remove(fn)
-    
     strain = TimeSeries.fetch_open_data(detector, t0-16, t0+16, cache=False)
     return strain
 
+st.sidebar.markdown("## Select Data Time and Detector")
 
-# -- Set a GPS time:
-st.sidebar.markdown("### Select detector and GPS time")
+# -- Get list of events
+# find_datasets(catalog='GWTC-1-confident',type='events')
+eventlist = datasets.find_datasets(type='events')
+eventlist = [name.split('-')[0] for name in eventlist if name[0:2] == 'GW']
+eventset = set([name for name in eventlist])
+eventlist = list(eventset)
+eventlist.sort()
 
+#-- Set time by GPS or event
+select_event = st.sidebar.selectbox('How do you want to find data?',
+                                    ['By event name', 'By GPS'])
+
+if select_event == 'By GPS':
+    # -- Set a GPS time:        
+    str_t0 = st.sidebar.text_input('GPS Time', '1126259462.4')    # -- GW150914
+    t0 = float(str_t0)
+
+    st.sidebar.markdown("""
+    Example times in the H1 detector:
+    * 1126259462.4    (GW150914) 
+    * 1187008882.4    (GW170817) 
+    * 933200215       (hardware injection)
+    * 1132401286.33   (Koi Fish Glitch) 
+    """)
+
+else:
+    chosen_event = st.sidebar.selectbox('Select Event', eventlist)
+    t0 = datasets.event_gps(chosen_event)
+    detectorlist = list(datasets.event_detectors(chosen_event))
+    
+    
 #-- Choose detector as H1, L1, or V1
-detector = st.sidebar.selectbox('Detector', ['H1', 'L1', 'V1'])
-
-str_t0 = st.sidebar.text_input('GPS Time', '1126259462.4')    # -- GW150914
-t0 = float(str_t0)
-
-st.sidebar.markdown("""
-Try these example times in the H1 detector:
- * 1126259462.4    (GW150914) 
- * 1187008882.4    (GW170817) 
- * 933200215       (hardware injection)
- * 1132401286.33   (Koi Fish Glitch) 
-""")
-
-
-
-
+detector = st.sidebar.selectbox('Detector', detectorlist)
 
 # Create a text element and let the reader know the data is loading.
 strain_load_state = st.text('Loading data...this may take a minute')
-strain = load_gw(t0, detector)
+try:
+    strain = load_gw(t0, detector)
+except:
+    st.text('Data load failed.  Try a different time and detector pair.')
+    raise st.ScriptRunner.StopException
+    
 strain_load_state.text('Loading data...done!')
 
 #-- Make a time series plot    
 
-st.markdown('### Raw data')
+st.subheader('Raw data')
 center = int(t0)
 strain = strain.crop(center-16, center+16)
 fig1 = strain.plot()
 st.pyplot(fig1)
-
 
 
 # -- Try whitened and band-passed plot
@@ -82,13 +91,14 @@ st.pyplot(fig3)
 st.subheader('Q-transform')
 
 st.sidebar.markdown('## Q-Transform Controls')
-dt = st.sidebar.slider('Time Range (s)', 0.1, 4.0, 1.0)  # min, max, default
+dtboth = st.sidebar.slider('Time Range (seconds)', 0.1, 8.0, 1.0)  # min, max, default
+dt = dtboth / 2.0
 vmax = st.sidebar.slider('Colorbar Max Energy', 10, 500, 25)  # min, max, default
 
 qcenter = st.sidebar.slider('Q-value', 5, 120, 5)  # min, max, default
 qrange = (int(qcenter*0.8), int(qcenter*1.2))
 
-# dt = 1  #-- Set width of q-transform plot, in seconds
+
 hq = strain.q_transform(outseg=(t0-dt, t0+dt), qrange=qrange)
 fig4 = hq.plot()
 ax = fig4.gca()
@@ -99,3 +109,9 @@ ax.set_ylim(bottom=15)
 st.pyplot(fig4)
 
 
+
+st.subheader("About this app")
+st.markdown("""
+This app displays data from LIGO, Virgo, and GEO downloaded from
+the Gravitational Wave Open Science Center at https://gw-openscience.org .
+""")
