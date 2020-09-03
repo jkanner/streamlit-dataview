3e50d7ca926d630df1db41a657e540fe496d39c5
Jonah Kanner <jkanner@caltech.edu>
2020-09-03 15:53:22 -0700

    sort detector list

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ else:
     chosen_event = st.sidebar.selectbox('Select Event', eventlist)
     t0 = datasets.event_gps(chosen_event)
     detectorlist = list(datasets.event_detectors(chosen_event))
-    
+    detectorlist.sort()
     
 #-- Choose detector as H1, L1, or V1
 detector = st.sidebar.selectbox('Detector', detectorlist)
