25ad57d2bcb9d5321bab6560cbbf6ad721fb4ec4
Jonah <jkanner@caltech.edu>
2023-03-14 12:10:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -47,12 +47,12 @@ st.markdown("""
  * Your plots will appear below
 """)
 
-@st.cache(ttl=3600, max_entries=5)   #-- Magic command to cache data
+@st.cache(max_entries=5)   #-- Magic command to cache data
 def load_gw(t0, detector, fs=4096):
     strain = TimeSeries.fetch_open_data(detector, t0-14, t0+14, sample_rate = fs, cache=False)
     return strain
 
-@st.cache(ttl=3600, max_entries=10)   #-- Magic command to cache data
+@st.cache(max_entries=10)   #-- Magic command to cache data
 def get_eventlist():
     allevents = datasets.find_datasets(type='events')
     eventset = set()
